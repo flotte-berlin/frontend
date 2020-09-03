acdf648fdf5c4d3caeefe8e0e77bfe48c25264ed
Max Ehrlicher-Schmidt <maxes2000@t-online.de>
2020-09-03 15:31:24 +0200

    Add routs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent} from './pages/login/login.component'
+import { BikesComponent} from './pages/bikes/bikes.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'bikes', component: BikesComponent },
+  { path: '', redirectTo: '/bikes', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  
+}
