3ae12966b6b965ed1a885613d2f71529617f5315
Max Ehrlicher-Schmidt <maxes2000@t-online.de>
2020-09-07 14:18:09 +0200

    Remove Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// Angular Material Components
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatTableModule} from '@angular/material/table';
-import {MatInputModule} from '@angular/material/input';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatIconModule} from '@angular/material/icon';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -31,13 +21,6 @@ import { BikesComponent } from './pages/bikes/bikes.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatTableModule,
-    MatInputModule,
-    MatMenuModule,
-    MatSlideToggleModule,
-    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
