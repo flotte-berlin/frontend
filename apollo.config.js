7d32b10740e7659c6bd8ecaa76b1e34b8dd00b93
Max Ehrlicher-Schmidt <maxes2000@t-online.de>
2020-09-18 18:07:43 +0200

    Fix apollo config

--- apollo.config.js
+++ apollo.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     client: {
+      excludes: ['src/generated/*'], // ignore the generated files
       service: {
         name: "flotte_project",
         url: "http://localhost:4000/graphql"
