4a3ffc23bd30647667b1d701a013ea74942baaf8
FlayInAHook <timmae2001@gmail.com>
2020-11-25 22:10:18 +0100

    added basic snackbar

---
+++ src/app/services/snackbar.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar'; 
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SnackBarService {
+
+    constructor(private snackBar : MatSnackBar) { } 
+  
+  
+    openSnackBar(message: string, action: string) { 
+        this.snackBar.open(message, action, { 
+            duration: 5000, 
+            panelClass: ['mat-toolbar', 'mat-primary']
+        });
+    } 
+}
