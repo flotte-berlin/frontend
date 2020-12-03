183b5347753127f93db3d7eacf871e609f18659f
Max Ehrlicher-Schmidt <maxes2000@t-online.de>
2020-12-03 23:17:55 +0100

    Seperated DateRange Inputs

--- src/app/components/tableComponents/date-range-cell/date-range-cell.component.ts
+++ src/app/components/tableComponents/date-range-cell/date-range-cell.component.ts
@@ -12,7 +12,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-date-range-cell',
   templateUrl: './date-range-cell.component.html',
-  styleUrls: ['./date-range-cell.component.scss']
+  styleUrls: ['./date-range-cell.component.scss'],
 })
 export class DateRangeCellComponent implements OnInit {
   @Input()
@@ -26,7 +26,7 @@ export class DateRangeCellComponent implements OnInit {
 
   @Input()
   editable = false;
-  
+
   @Input()
   required = false;
   @Input()
@@ -34,19 +34,58 @@ export class DateRangeCellComponent implements OnInit {
   @Output() validityChange = new EventEmitter<boolean>();
   isValid = true;
 
+  dateRangeGroup: FormGroup;
+
   constructor(private cdr: ChangeDetectorRef, public datepipe: DatePipe) {}
 
   ngOnInit() {
+    this.dateRangeGroup = new FormGroup({
+      from: new FormControl(),
+      to: new FormControl(),
+    });
+    this.dateRangeGroup.controls['from'].markAsTouched();
+    this.dateRangeGroup.controls['to'].markAsTouched();
+
+    if (this.editable) {
+    setTimeout(()=>this.checkIfDateRangeIsValid());
+    }
   }
 
   startDateChange(event): void {
     this.from = this.transformDate(event.value);
     this.fromChange.emit(this.from);
+    this.checkIfDateRangeIsValid();
   }
 
   endDateChange(event) {
     this.to = this.transformDate(event.value);
     this.toChange.emit(this.to);
+    this.checkIfDateRangeIsValid();
+  }
+
+  checkIfDateRangeIsValid() {
+    if (this.required && this.from == null) {
+      this.setRangeError(true);
+      return;
+    }
+    if (this.to == null || this.from == null) {
+      this.setRangeError(false);
+      return;
+    }
+    const isValid = new Date(this.to) >= new Date(this.from);
+    this.setRangeError(!isValid);
+  }
+
+  setRangeError(error: boolean): void {
+    console.log("error", error)
+    this.validityChange.emit(!error);
+    if (error) {
+      this.dateRangeGroup.controls['from'].setErrors({ rangeError: true });
+      this.dateRangeGroup.controls['to'].setErrors({ rangeError: true });
+    } else {
+      this.dateRangeGroup.controls['from'].setErrors(null);
+      this.dateRangeGroup.controls['to'].setErrors(null);
+    }
   }
 
   transformDate(date) {
@@ -54,10 +93,9 @@ export class DateRangeCellComponent implements OnInit {
   }
 
   toLocaleDate(date: string): string {
-    if (date == "") {
-      return "";
+    if (date == '') {
+      return '';
     }
     return new Date(date).toLocaleDateString();
   }
-
 }
