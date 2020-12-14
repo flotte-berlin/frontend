3a5fb44d67777c1bb53a9619f5ec26230b2fa354
FlayInAHook <timmae2001@gmail.com>
2020-12-14 15:25:15 +0100

    changed svg icon in table overview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { NavService } from './components/menu-list-item/nav.service';
 import { tableLinks } from 'src/app/tableLinks';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -28,12 +30,18 @@ export class AppComponent {
     private themeService: ColorThemeService,
     private authService: AuthService,
     private router: Router,
-    private navService: NavService
+    private navService: NavService,
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
   ) {
     this.renderer.addClass(document.body, 'mat-app-background'); //so the background color changes dependent on current theme
     this.themeService.load();
     this.darkThemeIsActive = this.themeService.currentActive() === 'dark-theme';
     this.authService.loggedIn.subscribe((value) => (this.loggedIn = value));
+    this.matIconRegistry.addSvgIcon(
+      "cargobike",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/cargobike.svg")
+    );
   }
 
   changeTheme(event) {
