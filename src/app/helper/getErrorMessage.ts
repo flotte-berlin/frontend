6023c847a0813015c142b9d107b276ac747ef042
Max Ehrlicher-Schmidt <maxes2000@t-online.de>
2020-12-26 22:51:29 +0100

    Add more custom error messages

--- src/app/helper/getErrorMessage.ts
+++ src/app/helper/getErrorMessage.ts
@@ -13,5 +13,8 @@ export function getErrorMessage(error: any): string {
     if (message.includes("TimeFrames with ids") && message.includes("overlapping")) {
         return "Überlappende Zeitscheibe - Ein Lastenrad kann zu einem Zeitpunkt nur an einem Standort stehen."
     }
+    if (message.includes("Engagements with ids") && message.includes("overlapping")) {
+        return "Überlappendes Engagement - Ein Lastenrad kann zu einem Zeitpunkt nur einem Aktiven über den selben Engagementtypen zugeordnet werden."
+    }
     return message || 'Es ist ein Fehler aufgetreten. Prüfen Sie Ihre Eingaben oder versuchen Sie es später erneut.';
 }
